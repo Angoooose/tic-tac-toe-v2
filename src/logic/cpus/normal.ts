1b2d5fb281919a2162106b096c4520fcb6deb922
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-01-01 22:02:41 -0600

    Adding randomness to normal difficulty CPU
    - 9/10 chance CPU sees winning move
    - 1/3 chance CPU sees defensive move
    
    Adding this as an attempt to make the CPU more human-like, as we're prone to errors.

--- src/logic/cpus/normal.ts
+++ src/logic/cpus/normal.ts
@@ -2,12 +2,23 @@ import BoardValue from '../../Types/BoardValue';
 import { Teams } from '../../Types/GameSettings';
 
 import checkWinningMove from '../checkWinningMove';
+import checkDefensiveMove from '../checkDefensiveMove';
 import findWinPath from '../findWinPath';
 import getRandomChoice from '../getRandomChoice';
 
 export default function normalCpuChoice(board: BoardValue[], playerTeam: Teams, cpuTeam: Teams) {
-    let winningMove = checkWinningMove(board, cpuTeam);
-    if (winningMove > -1) return winningMove;
+
+    if (Math.floor(Math.random() * 10) < 9) {
+        console.log('winning');
+        let winningMove = checkWinningMove(board, cpuTeam);
+        if (winningMove > -1) return winningMove;
+    }
+
+    if (Math.floor(Math.random() * 3) === 1) {
+        console.log('defense');
+        let defensiveMove = checkDefensiveMove(board, playerTeam);
+        if (defensiveMove > -1) return defensiveMove;
+    }
 
     let optimalWinPath = findWinPath(board, playerTeam);
     if (optimalWinPath > 0) return optimalWinPath;
