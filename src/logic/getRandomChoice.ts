e425f6a55f507f12b5ca517ede7cdb5b511cc23f
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-01-01 22:14:18 -0600

    Update getRandomChoice.ts

--- src/logic/getRandomChoice.ts
+++ src/logic/getRandomChoice.ts
@@ -3,7 +3,7 @@ import BoardValue from '../Types/BoardValue';
 export default function getRandomChoice(board: BoardValue[]): number {
     let randomSquare: number = -1;
     while (board[randomSquare] !== '') {
-        randomSquare = Math.floor(Math.random() * Math.floor(9));
+        randomSquare = Math.floor(Math.random() * 9);
         if (board[randomSquare] === '') {
             return randomSquare;
         }
